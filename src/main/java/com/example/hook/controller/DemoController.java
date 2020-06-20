5a1c0bffa430867777a1ed778ad35aed81ad2eeb
lcy2317 <lcy2317@users.noreply.github.com>
2020-06-20 16:16:30 +0800

    init

---
+++ src/main/java/com/example/hook/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.hook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description
+ * @Author lichengyang
+ * @Date 2020/6/18 22:18
+ */
+@RestController
+@RequestMapping("/ci")
+public class DemoController {
+    @RequestMapping("/get")
+    private String Foo(HttpServletRequest request) {
+        String clone_url = request.getParameter("clone_url");
+        return clone_url;
+    }
+}
---
+++ src/test/java/com/example/hook/HookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
